bbf5ca573fd715abfed48f9b0d981351c553d0de
darkydtm <boat-appear-portal@duck.com>
2026-08-01 03:08:23 +0500

    fix CI build warnings

--- kmp/core/build.gradle.kts
+++ kmp/core/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 kotlin {
-    androidLibrary {
+    android {
         namespace = "com.tonapps.compose.core"
         compileSdk = libs.versions.android.sdk.compile.get().toInt()
     }
